51f59e9a5a80b714c02086c1034c85c94660ca4e
BeastF5 <38111176+BeastF5@users.noreply.github.com>
2018-04-16 15:37:33 +0530

    Create termux-themer.sh

---
+++ termux-themer.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+function jsonValue() {
+    KEY=$1
+    num=$2
+    awk -F"[,:}]"
+'{for(i=1;i<=NF;i++){if($i~/'$KEY'\042/){print
+$(i+1)}}}' | tr -d '"' | sed -n ${num}p
+}
+if [ $# -eq 0 ]; then
+    echo Usage: $0 theme-name
+    echo
+    echo Themes:
+    curl -k -s
+"https://api.github.com/repos/termux/termux-styling/content$
+name|sed 's/.properties//g'|sed 's/ //g' else
+    cd ~
+    mkdir -p .termux
+    curl -k -s
+"https://raw.githubusercontent.com/termux/termux-styling/ma$
+> ~/.termux/colors.properties
+    termux-reload-settings
+fi
